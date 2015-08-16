e03ed3856367dd2617441f96baf3f18f1be8e411
Nate Brunette <n@tebru.net>
2015-08-16 11:31:17 -0500

    Adding additional assertions
    Signed-off-by: Nate Brunette <n@tebru.net>

--- src/assert.php
+++ src/assert.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * File assert.php
+ * File return assert.php
  */
 
 namespace Tebru;
 
 use Exception;
 use LogicException;
+use OutOfBoundsException;
 
 /**
  * Assert a condition
@@ -24,8 +25,251 @@ function assert($condition, Exception $exception = null)
     }
 
     if (null === $exception) {
-        $exception = new LogicException('Failed to assert condition');
+        $exception = new LogicException('Failed asserting condition');
     }
 
     throw $exception;
+}
+
+/**
+ * Simple assertion that allows changing the message
+ *
+ * Throws a LogicException on failure
+ *
+ * @param $condition
+ * @param null $message
+ * @param null $_
+ */
+function assertThat($condition, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 2);
+
+    if (null === $message) {
+        $message = 'Failed asserting condition';
+    }
+
+    assert($condition, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert two values are equal
+ *
+ * @param $expected
+ * @param $actual
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertEqual($expected, $actual, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    if (null === $message) {
+        $message = 'Failed asserting that "%s" equals "%s"';
+        $printArguments = [$actual, $expected];
+    }
+
+    return assert($expected == $actual, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert two values are not equal
+ *
+ * @param $expected
+ * @param $actual
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertNotEqual($expected, $actual, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    if (null === $message) {
+        $message = 'Failed asserting that "%s" does not equal "%s"';
+        $printArguments = [$actual, $expected];
+    }
+
+    return assert($expected != $actual, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert two values are the same
+ *
+ * @param $expected
+ * @param $actual
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertSame($expected, $actual, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    if (null === $message) {
+        $message = 'Failed asserting that "%s" is the same as "%s"';
+        $printArguments = [$actual, $expected];
+    }
+
+    return assert($expected === $actual, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert two values are not the same
+ *
+ * @param $expected
+ * @param $actual
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertNotSame($expected, $actual, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    if (null === $message) {
+        $message = 'Failed asserting that "%s" is not the same as "%s"';
+        $printArguments = [$actual, $expected];
+    }
+
+    return assert($expected !== $actual, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert value is true
+ *
+ * @param $value
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertTrue($value, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 2);
+
+    if (null === $message) {
+        $message = 'Failed asserting true';
+    }
+
+    return assert(true === $value, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert value is false
+ *
+ * @param $value
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertFalse($value, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 2);
+
+    if (null === $message) {
+        $message = 'Failed asserting false';
+    }
+
+    return assert(false === $value, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert value is null
+ *
+ * @param $value
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertNull($value, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 2);
+
+    if (null === $message) {
+        $message = 'Failed asserting null';
+    }
+
+    return assert(null === $value, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert value is not null
+ *
+ * @param $value
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertNotNull($value, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 2);
+
+    if (null === $message) {
+        $message = 'Failed asserting not null';
+    }
+
+    return assert(null !== $value, new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert that the key exists in the array
+ *
+ * @param $key
+ * @param array $search
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertArrayKeyExists($key, array $search, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    if (null === $message) {
+        $message = 'Failed asserting that key "%s" exists in the array';
+        $printArguments = [$key];
+    }
+
+    return assert(array_key_exists($key, $search), new OutOfBoundsException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert that a key does not exist in array
+ * @param $key
+ * @param array $search
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertArrayKeyNotExists($key, array $search, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    if (null === $message) {
+        $message = 'Failed asserting that key "%s" does not exist in the array';
+        $printArguments = [$key];
+    }
+
+    return assert(!array_key_exists($key, $search), new LogicException(vsprintf($message, $printArguments)));
+}
+
+/**
+ * Assert correct number of elements
+ *
+ * @param $expected
+ * @param $countable
+ * @param null $message
+ * @param null $_
+ * @return null
+ */
+function assertCount($expected, $countable, $message = null, $_ = null)
+{
+    $printArguments = array_slice(func_get_args(), 3);
+
+    $numElements = count($countable);
+    if (null === $message) {
+        $message = 'Failed asserting that "%d" elements exist, found "%d"';
+        $printArguments = [$expected, $numElements];
+    }
+
+    return assert($expected === $numElements, new \LengthException(vsprintf($message, $printArguments)));
 }
--- tests/AssertTest.php
+++ tests/AssertTest.php
@@ -15,25 +15,312 @@ use Tebru;
  */
 class AssertTest extends PHPUnit_Framework_TestCase
 {
-    public function testAssertTrue()
-    {
-        Tebru\assert(true);
-    }
-
     /**
      * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting condition
      */
-    public function testAssertFalse()
+    public function testAssertConditionFalse()
     {
         Tebru\assert(false);
     }
 
+    public function testAssertConditionTrue()
+    {
+        $this->assertNull(Tebru\assert(true));
+    }
+
     /**
      * @expectedException \OutOfRangeException
+     * @expectedExceptionMessage My test message
      */
-    public function testChangeException()
+    public function testAssertConditionChangeException()
     {
         $array = [0];
-        Tebru\assert(isset($array['key']), new \OutOfRangeException());
+        Tebru\assert(isset($array['key']), new \OutOfRangeException('My test message'));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting condition
+     */
+    public function testAssertThatConditionFalse()
+    {
+        Tebru\assertThat(false);
+    }
+
+    public function testAssertThatConditionTrue()
+    {
+        $this->assertNull(Tebru\assertThat(true));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertThatConditionMessage()
+    {
+        Tebru\assertThat(false, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting that "2" equals "1"
+     */
+    public function testAssertEqualFalse()
+    {
+        Tebru\assertEqual(1, 2);
+    }
+
+    public function testAssertEqualTrue()
+    {
+        $this->assertNull(Tebru\assertEqual(1, '1'));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertEqualMessage()
+    {
+        Tebru\assertEqual(1, 2, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting that "1" does not equal "1"
+     */
+    public function testAssertNotEqualFalse()
+    {
+        Tebru\assertNotEqual(1, '1');
+    }
+
+    public function testAssertNotEqualTrue()
+    {
+        $this->assertNull(Tebru\assertNotEqual(1, 2));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertNotEqualMessage()
+    {
+        Tebru\assertNotEqual(1, '1', 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting that "1" is the same as "1"
+     */
+    public function testAssertSameFalse()
+    {
+        Tebru\assertSame(1, '1');
+    }
+
+    public function testAssertSameTrue()
+    {
+        $this->assertNull(Tebru\assertSame(1, 1));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertSameMessage()
+    {
+        Tebru\assertSame(1, 2, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting that "1" is not the same as "1"
+     */
+    public function testAssertNotSameFalse()
+    {
+        Tebru\assertNotSame(1, 1);
+    }
+
+    public function testAssertNotSameTrue()
+    {
+        $this->assertNull(Tebru\assertNotSame(1, '1'));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertNotSameMessage()
+    {
+        Tebru\assertNotSame(1, 1, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting true
+     */
+    public function testAssertTrueFalse()
+    {
+        Tebru\assertTrue(false);
+    }
+
+    public function testAssertTrueTrue()
+    {
+        $this->assertNull(Tebru\assertTrue(true));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertTrueMessage()
+    {
+        Tebru\assertTrue(false, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting false
+     */
+    public function testAssertFalseFalse()
+    {
+        Tebru\assertFalse(true);
+    }
+
+    public function testAssertFalseTrue()
+    {
+        $this->assertNull(Tebru\assertFalse(false));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertFalseMessage()
+    {
+        Tebru\assertFalse(true, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting null
+     */
+    public function testAssertNullFalse()
+    {
+        Tebru\assertNull(1);
+    }
+
+    public function testAssertNullTrue()
+    {
+        $this->assertNull(Tebru\assertNull(null));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertNullMessage()
+    {
+        Tebru\assertNull(1, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting not null
+     */
+    public function testAssertNotNullFalse()
+    {
+        Tebru\assertNotNull(null);
+    }
+
+    public function testAssertNotNullTrue()
+    {
+        $this->assertNull(Tebru\assertNotNull(1));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertNotNullMessage()
+    {
+        Tebru\assertNotNull(null, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @expectedExceptionMessage Failed asserting that key "1" exists in the array
+     */
+    public function testAssertArrayKeyExistsFalse()
+    {
+        $array[] = 'test';
+        Tebru\assertArrayKeyExists(1, $array);
+    }
+
+    public function testAssertArrayKeyExistsTrue()
+    {
+        $array[] = 'test';
+        $this->assertNull(Tebru\assertArrayKeyExists(0, $array));
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertArrayKeyExistsMessage()
+    {
+        $array[] = 'test';
+        Tebru\assertArrayKeyExists(1, $array, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Failed asserting that key "0" does not exist in the array
+     */
+    public function testAssertArrayKeyNotExistsFalse()
+    {
+        $array[] = 'test';
+        Tebru\assertArrayKeyNotExists(0, $array);
+    }
+
+    public function testAssertArrayKeyNotExistsTrue()
+    {
+        $array[] = 'test';
+        $this->assertNull(Tebru\assertArrayKeyNotExists(1, $array));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertArrayKeyNotExistsMessage()
+    {
+        $array[] = 'test';
+        Tebru\assertArrayKeyNotExists(0, $array, 'My %s message', 'test');
+    }
+
+    /**
+     * @expectedException \LengthException
+     * @expectedExceptionMessage Failed asserting that "0" elements exist, found "1"
+     */
+    public function testAssertCountFalse()
+    {
+        $array[] = 'test';
+        Tebru\assertCount(0, $array);
+    }
+
+    public function testAssertCountTrue()
+    {
+        $array[] = 'test';
+        $this->assertNull(Tebru\assertCount(1, $array));
+    }
+
+    /**
+     * @expectedException \LengthException
+     * @expectedExceptionMessage My test message
+     */
+    public function testAssertCountMessage()
+    {
+        $array[] = 'test';
+        Tebru\assertCount(0, $array, 'My %s message', 'test');
     }
 }
